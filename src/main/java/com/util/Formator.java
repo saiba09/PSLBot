f8fd5ac572f6b4fa639f0d42a6f383bf98467540
saiba09 <saiba_sheikh@persistent.co.in>
2017-12-01 13:23:50 +0530

    1 dec - 1.2

--- src/main/java/com/util/Formator.java
+++ src/main/java/com/util/Formator.java
@@ -1,5 +1,6 @@
 package com.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Logger;
@@ -7,7 +8,13 @@ import java.util.logging.Logger;
 public class Formator {
 	private static final Logger log = Logger.getLogger(Formator.class.getName());
 	public static String getFormatedDate(String dateString){
-		Date date = new Date(dateString);
+		Date date = null;
+		try {
+			date = new SimpleDateFormat("yyyy-MM-dd").parse(dateString);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return getFormatedDate(date);
 	}
 	public static String getFormatedDate(Date date){
